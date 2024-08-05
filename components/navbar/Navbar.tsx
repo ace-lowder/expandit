4af804a366b78cb8563ac9621ebf84e2dffec80f
Ace <ace.lowder@gmail.com>
2024-08-05 15:10:29 -0700

    Refactor navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,55 +2,19 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "@/public/logo.svg";
+import { menuItems, renderMenuItem } from "@/constants/menuItems";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Menu items for desktop and mobile
-  const menuItems = [
-    { href: "/expand", label: "Expand", position: "left" },
-    { href: "/pricing", label: "Pricing", position: "left" },
-    { href: "/login", label: "Log In", position: "right" },
-    { href: "/signup", label: "Sign Up", position: "right", button: true },
-  ];
-
-  const renderMenuItem = ({
-    href,
-    label,
-    button,
-  }: {
-    href: string;
-    label: string;
-    button?: boolean;
-  }) => {
-    return button ? (
-      <Link
-        key={href}
-        href={href}
-        className="bg-gray-300 text-gray-700 hover:bg-gray-400 transition duration-300 px-6 py-2 rounded-full"
-      >
-        {label}
-      </Link>
-    ) : (
-      <Link
-        key={href}
-        href={href}
-        className="text-gray-700 hover:bg-gray-200 transition duration-300 px-3 py-1 rounded"
-      >
-        {label}
-      </Link>
-    );
-  };
-
   return (
     <nav className="bg-white shadow-md border-b border-gray-100 flex items-center justify-between px-4 py-3 gap-8">
       {/* Logo and App Name */}
-      <Link href="/" className="flex items-center space-x-4">
+      <Link href="/" className="flex items-center space-x-3">
         <Image src={logo} alt="Expandit Logo" width={40} height={40} />
-        <span className="text-2xl font-bold text-gray-900">Expandit</span>
+        <span className="text-2xl font-bold text-gray-900 mt-[2px]">
+          Expandit
+        </span>
       </Link>
 
       {/* Desktop Menu */}
@@ -70,33 +34,8 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* Mobile Menu Button */}
-      <button
-        className="md:hidden text-gray-700 focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <FaBars className="w-6 h-6" />
-      </button>
-
-      {/* Mobile Menu */}
-      <div
-        className={`md:hidden fixed inset-0 bg-white z-50 transition-transform transform ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        {/* Close Button */}
-        <button
-          className="absolute top-5 right-4 text-gray-700 focus:outline-none"
-          onClick={() => setIsOpen(false)}
-        >
-          <FaTimes className="w-6 h-6" />
-        </button>
-
-        {/* Menu Items */}
-        <div className="flex flex-col items-center space-y-6 mt-32">
-          {menuItems.map(renderMenuItem)}
-        </div>
-      </div>
+      {/* Mobile Navbar */}
+      <MobileNavbar />
     </nav>
   );
 };
